12d9e960faa57291e2565169d7e93ee1852140ab
Hasin Hayder <countdraculla@gmail.com>
2023-12-25 22:29:35 +0600

    Code

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Tagline from './components/Tagline.vue';
+import Hero from './components/Hero.vue';
+import VideoButton from './components/VideoButton.vue';
+import LogoSection from './components/LogoSection.vue';
+import svgIcon from './assets/vue.svg'
+</script>
+
+<template>
+  <section class="bg-white ">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+      <Tagline/>
+      <Hero/>
+      <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+        <VideoButton/>
+        <video-button/>
+      </div>
+      <LogoSection/>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/LogoSection.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import Logo1 from './Logo1.vue';
+    import Logo2 from './Logo2.vue';
+    import Logo3 from './Logo3.vue';
+</script>
+ 
+<template>
+    <div class="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+        <span class="font-semibold text-gray-400 uppercase">FEATURED IN</span>
+        <div class="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
+            <Logo1/>
+            <Logo2/>
+            <Logo3/>
+        </div>
+    </div>
+</template>
+ 
+<style scoped></style>
